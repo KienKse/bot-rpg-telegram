8aa7eea1daa055d069fc449815b4b0c4fd4b4a63
Ícaro Santana Silva <icaro.silva@solutis.com.br>
2021-04-27 18:35:55 -0300

    testing process acess to enviroments

--- singletonBot.js
+++ singletonBot.js
@@ -1,8 +1,7 @@
 const TelegramBot = require( `node-telegram-bot-api` );
-require("dotenv").config();
+// require("dotenv").config();
 
-const TOKEN = ENV['TOKEN'];
-// const TOKEN = process.env.TOKEN;ENV['URI']
+const TOKEN = process.env.TOKEN;
 
 var singletonBot = (() => {
   var instance;
