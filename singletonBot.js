bab890dfe8c307c28bf0bbdcf99be6b3c10e89e6
Ícaro Santana Silva <icaro.silva@solutis.com.br>
2021-04-29 11:54:56 -0300

    adding webHook to bot instance and config dotent uncommented

--- singletonBot.js
+++ singletonBot.js
@@ -1,15 +1,17 @@
 const TelegramBot = require( `node-telegram-bot-api` );
-// require("dotenv").config();
+require("dotenv").config();
 
 const TOKEN = process.env.TOKEN;
 const PORT = process.env.PORT || 5000;
 
 var singletonBot = (() => {
   var instance;
-
   function createInstance() {
     let bot = new TelegramBot( TOKEN, { webHook: { port: PORT }});
-      return bot;
+    console.log(`POLLING: ${bot.options.polling}`);
+    console.log(`PORT: ${PORT}`);
+    console.log(`HOST: ${HOST}`);
+    return bot;
   }
 
   return {
