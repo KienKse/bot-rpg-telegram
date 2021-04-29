16d11f202a56e111ff3188a4909aad2725e13e26
KienKse <icaro.silva@solutis.com.br>
2021-04-29 12:44:59 -0300

    console verifying options on bot instance

--- singletonBot.js
+++ singletonBot.js
@@ -2,14 +2,18 @@ const TelegramBot = require( `node-telegram-bot-api` );
 // require("dotenv").config();
 
 const TOKEN = process.env.TOKEN;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8432;
+const HOST = process.env.HOST || '0.0.0.0';
 const ENVIRONMENT = process.env.ENVIRONMENT;
 
 var singletonBot = (() => {
   var instance;
   function createInstance() {
-    let bot = new TelegramBot( TOKEN, ENVIRONMENT == 'PROD' ? { webHook: { port: PORT }} : { polling: true});
+    let bot = new TelegramBot( TOKEN, ENVIRONMENT == 'PROD' ? { webHook: { port: PORT, host: HOST }} : { polling: true});
+    console.log(`OPTIONS PORT: ${bot.options.webHook.port}`);
+    console.log(`OPTIONS HOST: ${bot.options.webHook.host}`);
     console.log(`PORT: ${PORT}`);
+    console.log(`HOST: ${HOST}`);
     console.log(`ENVIRONMENT: ${ENVIRONMENT}`);
     console.log(`POLLING: ${bot.options.polling}`);
     return bot;
